0c5bf4e6b5e4f72ec28a559707be18090b5707fc
David McCanney <david@mccanney.org>
2018-05-12 21:21:47 +0100

    Change variable description
    * Small changes to the text of the input variables description.

--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 variable "domains" {
-  type        = "list"
-  description = "A list of domains to create the CloudFront infrastructure for. "
-  default     = []
+    type        = "list"
+    description = "A list of domains to be built into a static website."
+    default     = []
 }
 
 variable "secret" {
-  type        = "string"
-  description = "A secret string between CloudFront and S3 to control access."
-  default     = ""
+    type        = "string"
+    description = "A secret string between CloudFront and S3 to control access."
+    default     = ""
 }
 
 variable "www_is_main" {
-  type        = "string"
-  description = "Controls whether the naked domain or www subdomain is the main site."
-  default     = false
+    type        = "string"
+    description = "Controls whether the naked domain or www subdomain is the main site."
+    default     = false
 }
 
 variable "enable_iam_user" {
-  type        = "string"
-  description = "Controls whether the module should create the AWS IAM deployment user."
-  default     = true
+    type        = "string"
+    description = "Controls whether the module should create the AWS IAM deployment user."
+    default     = true
 }
 
 variable "cdn_settings" {
-  type        = "map"
-  description = "CloudFront CDN settings."
-  default     = {}
+    type        = "map"
+    description = "A map containing configurable CloudFront CDN settings."
+    default     = {}
 }
 
 variable "countries" {
-  type        = "list"
-  description = "The ISO 3166-alpha-2 country codes of the countries to be black- or white-listed."
-  default     = []
+    type        = "list"
+    description = "The ISO 3166-alpha-2 country codes of the countries to be allowed or restricted."
+    default     = []
 }
